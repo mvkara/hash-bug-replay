c5f16e790ce88f2822ae9ab349e23f5a7ca62fe5
mvkra <mvkra@noreply.github.com>
2023-02-12 09:20:42 +1100

    reply with F# 7

---
+++ Program.fs
@@ -0,0 +1,9 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+open System
+open FSharp.HashCollections
+
+let s = HashSet.ofSeq [ 1 .. 10 ]
+
+printfn "Hashset contains 6 [%b]" (s |> HashSet.contains 6)
+
+printfn "Hashset contains 20 [%b]" (s |> HashSet.contains 20)
